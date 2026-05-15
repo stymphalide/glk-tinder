60b70e111372a9cae3dc6f3ff48888683cb1a8b2
stymphalide <stymphalide@gmx.net>
2026-05-15 10:56:47 +0200

    Add tests for file1

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,39 @@
-from glk_tinder.main import add
+from glk_tinder.main import *
 
-def test_add():
-    assert add(1, 2) == 3
+import subprocess
+from pathlib import Path
+
+
+def test_main(tmp_path):
+    input_file = Path("tests/data/input_file1.csv")
+    output_file = tmp_path / "out.csv"
+    
+    main(input_file, output_file)
+
+    actual = output_file.read_text()
+
+    expected = Path("tests/data/expected_file1.csv").read_text()
+    assert actual == expected
+
+
+def test_cli(tmp_path):
+    input_file = Path("tests/data/input_file1.csv")
+    output_file = tmp_path / "out.csv"
+
+    subprocess.run(
+        [
+            "python",
+            "-m",
+            "glk_tinder",
+            "--input",
+            str(input_file),
+            "--output",
+            str(output_file),
+        ],
+        check=True,
+    )
+
+    expected = Path("tests/data/expected_file1.csv").read_text()
+    actual = output_file.read_text()
+
+    assert actual == expected
