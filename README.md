35467924150e04f67c8fdce95758f782c103b766
stymphalide <stymphalide@gmx.net>
2026-06-17 14:04:06 +0200

    Add constraint argument to program.

--- README.md
+++ README.md
@@ -7,5 +7,10 @@ python -m glk_tinder --input tests/data/input_file1.csv --output out.csv
 ```
 
 ```bash
+python -m glk_tinder --input tests/data/input_file1.csv --output out.csv --constraints tests/data/constraints1.yaml
+```
+
+
+```bash
 python -m glk_tinder --input tests/data/input_glk.csv --output out.csv
 ```
--- src/glk_tinder/__main__.py
+++ src/glk_tinder/__main__.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process input files")
     parser.add_argument("--input", required=True, help="Path to input CSV file")
     parser.add_argument("--output", required=True, help="Path to output CSV file")
+    parser.add_argument("--constraints", required=False, help="Path to constraints YAML file")
     args = parser.parse_args()
-    main(args.input, args.output)
+    main(args.input, args.output, args.constraints)
